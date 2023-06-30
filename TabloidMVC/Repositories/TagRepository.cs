4e80fcd35e53920c6ebcc0ff064d420d7bcea616
Timothy Clark <TimDMClark@gmail.com>
2023-06-30 15:26:29 -0400

    Added Ability to View All Tags

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Data.SqlClient;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly IConfiguration _config;
+
+        public TagRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Tag> GetAllTags()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Tag.Id, Tag.Name
+                        FROM Tag";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Tag> dogs = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        Tag tag = new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+
+                        dogs.Add(tag);
+                    }
+                    reader.Close();
+
+                    return dogs;
+                }
+            }
+        }
+        public Tag GetTagById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Tag.Id, Tag.Name
+                        FROM Tag
+                        WHERE Tag.Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Tag tag = null;
+
+                    if (reader.Read())
+                    {
+                        tag = new Tag
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+
+                        reader.Close();
+
+                        return tag;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
