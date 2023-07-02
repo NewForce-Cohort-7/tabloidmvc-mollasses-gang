7f49f28424fcaf722b6dec17c0c6d16058c58c63
Gloria Reina <gloria.l.reina11@gmail.com>
2023-07-02 10:53:17 -0400

    Added features to edit/delete a post

--- TabloidMVC/Repositories/IPostRepository.cs
+++ TabloidMVC/Repositories/IPostRepository.cs
@@ -6,6 +6,8 @@ namespace TabloidMVC.Repositories
     public interface IPostRepository
     {
         void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
         List<Post> GetAllPublishedPosts();
         Post GetPublishedPostById(int id);
         Post GetUserPostById(int id, int userProfileId);
