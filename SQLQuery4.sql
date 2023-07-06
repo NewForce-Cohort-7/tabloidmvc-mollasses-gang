bfb04c4530f078e9a19c835c5d2d732397f0e523
Amanda Wright <125294724+amandakwright1@users.noreply.github.com>
2023-07-06 09:09:01 -0400

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,1 +1,2 @@
-﻿Select * From Comment;
+Set IDENTITY_INSERT [Comment] ON
+INSERT INTO Comment (Id,PostId,UserProfileId, Subject,Content, CreateDateTime) VALUES (1, 1,1,'test','testing', '2023/06/29'); Set IDENTITY_INSERT [Comment] OFF
